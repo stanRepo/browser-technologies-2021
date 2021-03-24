3168062966cfdeffc17d7f73f1dbf03a36de7983
Stan Ooms <Stanooms96@gmail.com>
2021-03-24 10:20:45 +0100

    updated flow

--- Eindopdracht/routes/loginAdmin.js
+++ Eindopdracht/routes/loginAdmin.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 router.get("/", function (req, res, next) {
   console.log("adminroute accessed");
+  console.log(req.body.username);
+  console.log(req.body.pw);
   if ((req.body.username === "admin" && req.body.pw === "0000") || 0000) {
-    res.render("admin");
+    res.render("admin.ejs");
   } else {
-    res.render("adminLoginFailed");
+    res.render("adminLoginFailed.ejs");
   }
 });
 
