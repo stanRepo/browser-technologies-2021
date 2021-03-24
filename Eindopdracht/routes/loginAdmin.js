4eb081867f4b29242e2feed1c4d625158d49cb9d
Stan Ooms <Stanooms96@gmail.com>
2021-03-24 11:18:14 +0100

    fixed routing on admin login pages

--- Eindopdracht/routes/loginAdmin.js
+++ Eindopdracht/routes/loginAdmin.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.post("/", function (req, res, next) {
   console.log("adminroute accessed");
   console.log(req.body.username);
   console.log(req.body.pw);
+
   if ((req.body.username === "admin" && req.body.pw === "0000") || 0000) {
-    res.render("admin.ejs");
+    res.render("admin");
   } else {
-    res.render("adminLoginFailed.ejs");
+    res.render("adminLoginFailed");
   }
 });
 
