fc30dbd9ab1d4430ac53754c1eb26ed689e21501
Stan Ooms <Stanooms96@gmail.com>
2021-03-25 23:35:56 +0100

    styled

--- Eindopdracht/public/javascripts/main.js
+++ Eindopdracht/public/javascripts/main.js
@@ -1,6 +1,7 @@
 import progressBarFeature from "./progressiveEnhancements/progressBar.js";
 import localStorageFeature from "./progressiveEnhancements/localStorage.js";
 import rangeElementsFeature from "./progressiveEnhancements/rangeElements.js";
+import onlineStatusFeature from "./progressiveEnhancements/onlineStatus.js";
 
 const activateFeatures = {
   progressBar: () => {
@@ -16,8 +17,14 @@ const activateFeatures = {
   rangeElements: () => {
     rangeElementsFeature();
   },
+  onlineStatus: () => {
+    if ("navigator" in window) {
+      onlineStatusFeature();
+    }
+  },
 };
 
 activateFeatures.rangeElements();
 activateFeatures.progressBar();
 activateFeatures.localStorage();
+activateFeatures.onlineStatus();
